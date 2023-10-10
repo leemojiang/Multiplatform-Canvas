f932e88a25cc7c4cb258764caa2d3758ec5a8b9e
R9000p <126@126.com>
2023-10-10 21:11:08 +0800

    失败版本

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request,Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import hashlib
@@ -7,22 +8,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="./templates")
-class Password(BaseModel):
-    password: str
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/login")
-async def login(password: Password):
+async def login(password: Annotated[str, Form()]):
     # Encrypt the password using a hashing algorithm (e.g., SHA256)
     # encrypted_id = hashlib.sha256(password.encode()).hexdigest()
     # Redirect the user to a page with the encrypted ID
-    return {"item_id": password}
+    # return {"item_id": password}
     
-    return {"redirect": f"/items/{encrypted_id}"}
+    return {"status":1,"redirect": f"/items/{password}"}
 
-@app.get("/items/{item_id}")
+@app.get("/item/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
